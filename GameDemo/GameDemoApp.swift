f39c7e0835661ea0c0d2acab6f4cfc4c2b6604bc
Anmay Gupta <anmaygupta1@gmail.com>
2022-01-13 13:52:48 -0600

    Initial Commit

---
+++ GameDemo/GameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDemoApp.swift
+//  GameDemo
+//
+//  Created by Anmay Gupta on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
